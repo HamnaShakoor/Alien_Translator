6ba6bdf1f4a716116f19936031ed2e754370c942
AHMAD <ahmmadshakoor35@gmail.com>
2026-05-29 17:56:59 +0500

    Updated my logic

--- main.cpp
+++ main.cpp
@@ -4,16 +4,15 @@
 using namespace std;
 const char english_lower[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 const char english_upper[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-const char alien_lower[26]={'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j'};
-const char alien_upper[26]={'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J'};
+const char alien_lower[26]={'a','b','o','d','e','f','g','r','i','j','k','l','m','n','u','p','q','h','s','t','o','v','w','x','y','z'};
+const char alien_upper[26]={'A','B','O','D','E','F','G','R','I','J','K','L','M','N','U','P','Q','H','S','T','O','V','W','X','Y','Z'};
 int main()
 {
     string text;
     while(true)
     {
-      cout<<"Please enter the your word or string in Alien Language: ";
+      cout<<" Please enter the your word or string in English Language: ";
       getline(cin, text);
-      cout<<endl;
       bool correctInput=true;
       if(text.length()==0)
       {
@@ -28,10 +27,9 @@ int main()
            continue;
          }
          bool found = false;
-          // alien lower check
          for(int j = 0; j < 26; j++)
          {
-            if(c == alien_lower[j] || c == alien_upper[j])
+            if(c == english_lower[j] || c == english_upper[j])
             {
                 found = true;
                 break;
@@ -49,9 +47,46 @@ int main()
      }
      else
      {
-        cout<<"Invalid input. Try Agian.";
+        cout<<endl;
+        cout<<"Invalid input. Try Agian."<<endl;
      }
     }
-    
     cout<<"Valid Input";
+    cout<<endl;
+    cout<<"Translated in Alien's Language: "<<endl;
+    string translation;
+    int length=text.length();
+    for (int i=0; i<length; i++)
+      {
+         char c=text[i];
+         if(c==' ')
+         { 
+          translation +=' ';
+          continue;
+         }
+         bool translated = false;
+         for(int j = 0; j < 26; j++)
+         {
+            if(c == english_lower[j])
+            {
+              translation +=alien_lower[j];
+              translated = true;
+              break;
+            }
+         }
+         if(translated==false)
+         {
+          for(int j = 0; j < 26; j++)
+          {
+            if(c == english_upper[j])
+            {
+              translation+=alien_upper[j];
+              translated = true;
+              break;
+            }
+          }
+         }
+      }
+      cout<<translation;
+      cout<<endl<<endl;
 }
